78b5a01adbb906ea6f5c50e965b4c4005a9bbabd
vikekh <vieekk@gmail.com>
2014-03-08 03:05:14 +0100

    Using onload now, but multiple instances compromise each other

--- src/jquery.compare.js
+++ src/jquery.compare.js
@@ -1,8 +1,77 @@
-(function ($) {
+(function ($, compare) {
+	var height,
+		settings,
+		width,
+		x,
+		$before,
+		$container,
+		$after;
+
+	compare.init = function (defaults, options) {
+		$container = $(this);
+		$before = $container.children('img.before'),
+		$after = $container.children('img.after');
+		settings = $.extend({}, defaults, options);
+
+		$before.on('load', function () {
+			//validate();
+			width = $before.width();
+			height = $before.height();
+			$container.width(width).height(height);
+			fold();
+		});
+	}
+
+	var applyCss = function () {};
+
+	var fold = function () {
+		switch (settings.direction) {
+			case 'vertical':
+				break;
+
+			case 'horizontal':
+			default:
+				x = Math.ceil(width/2);
+				$after.css('left', x + 'px');
+				break;
+		}
+	};
+
+	var validate = function () {
+		if ($before.length === 0) {
+			$.error('Could not find a before image.')
+		}
+
+		if ($after.length === 0) {
+			$.error('Could not find an after image.')
+		}
+
+		if ($before.length > 1) {
+			$.error('Too many before images, using first.')
+			$before = $before.first().nextAll().remove();
+		}
+
+		if ($after.length > 1) {
+			$.error('Too many after images, using first.')
+			$after = $after.first().nextAll().remove();
+		}
+
+		if ($before.next() !== $after) {
+			//$.error('Before and after images should be placed after each other in the DOM tree.');
+		}
+
+		if ($before.width() !== $after.width() || $before.height() !== $after.height()) {
+			$.error('Before and after images should have the same dimensions.');
+		}
+	};
+
+})(jQuery, window.compare = window.compare || {});
+
+(function ($, compare) {
 
 	$.fn.compare = function (options) {
 		return this.each(function () {
-			compare.init.apply(this, options);
+			compare.init.apply(this, $.fn.compare.defaults, options);
 		});
 	};
 
@@ -10,85 +79,8 @@
 		direction: 'horizontal'
 	};
 
-	var compare = (function () {
-		var settings,
-			width,
-			height,
-			x,
-			$before,
-			$after;
+})(jQuery, compare);
 
-		var init = function (options) {
-			settings = $.extend({}, $.fn.compare.defaults, options);
-			$before = $(this).children('img.before'),
-			$after = $(this).children('img.after');
-
-			validate();
-
-			width = $before.width();
-			height = $before.height();
-			$(this).width(width).height(height).css('position', 'relative');
-
-			fold();
-		}
-
-		var fold = function () {
-			switch (settings.direction) {
-				case 'vertical':
-					break;
-
-				case 'horizontal':
-				default:
-					x = Math.ceil(width/2);
-					$before.width(x).css({
-						'position': 'absolute',
-						'top': '0',
-						'left': '0',
-						'clip': 'rect(0, ' + x + 'px, ' + height + 'px, 0)'
-					});
-					$after.width(width - x).css({
-						'position': 'absolute',
-						'top': '0',
-						'left': x + 'px',
-						'clip': 'rect(0, ' + width + 'px, ' + height + 'px, ' + x + 'px)'
-					});
-					break;
-			}
-		};
-
-		var validate = function () {
-			if ($before.length === 0) {
-				$.error('Could not find a before image.')
-			}
-
-			if ($after.length === 0) {
-				$.error('Could not find an after image.')
-			}
-
-			if ($before.length > 1) {
-				$.error('Too many before images, using first.')
-				$before = $before.first().nextAll().remove();
-			}
-
-			if ($after.length > 1) {
-				$.error('Too many after images, using first.')
-				$after = $after.first().nextAll().remove();
-			}
-
-			if ($before.next() !== $after) {
-				//$.error('Before and after images should be placed after each other in the DOM tree.');
-			}
-
-			if ($before.width() !== $after.width() || $before.height() !== $after.height()) {
-				$.error('Before and after images should have the same dimensions.');
-			}
-		};
-
-		return {
-			init: init
-		};
-	})();
-
-})(jQuery);
-
-$('.jquery-compare').compare();
+jQuery(document).ready(function ($) {
+	$('.jquery-compare').compare();
+});
