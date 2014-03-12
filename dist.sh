cd8d8fff97f310be39e8db958ecdb3c36951bfe8
vikekh <vieekk@gmail.com>
2014-03-12 01:14:06 +0100

    Intelligent dist script

--- dist.sh
+++ dist.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
+# https://gist.github.com/cjus/1047794
+function jsonval {
+	echo $1 | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $2| cut -d":" -f2| sed -e 's/^ *//g' -e 's/ *$//g'
+	echo ${temp##*|}
+}
+
+json=`cat comparison.jquery.json`
+version=`jsonval "$json" version`
+info="/* jQuery Comparison v$version */"
+
 cp src/css/jquery.comparison.css dist/css/jquery.comparison.css
-java -jar yuicompressor.jar -o dist/css/jquery.comparison.min.css dist/css/jquery.comparison.css
+echo "$info" > dist/css/jquery.comparison.min.css
+java -jar yuicompressor.jar dist/css/jquery.comparison.css >> dist/css/jquery.comparison.min.css
+
 cp src/js/jquery.comparison.js dist/js/jquery.comparison.js
-java -jar yuicompressor.jar -o dist/js/jquery.comparison.min.js dist/js/jquery.comparison.js
+echo "$info" > dist/js/jquery.comparison.min.js
+java -jar yuicompressor.jar dist/js/jquery.comparison.js >> dist/js/jquery.comparison.min.js
